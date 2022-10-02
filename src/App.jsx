878623c8e5c6cd1c81484c477d8bf13c5f767f67
Raaydon <69201936+Raaydon@users.noreply.github.com>
2022-10-02 20:55:36 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState, useEffect, useRef } from "react";
 import { gsap } from "gsap";
+import $ from "jquery";
 
 import { ContextProvider } from "./context/context";
 // import Cursor from "./components/cursor";
 
 import Homepage from "./components/Homepage";
 import Nav from "./components/nav";
+import HamburgerNav from "./components/HamburgerNav";
 
 function App() {
 	const [loading, setLoading] = useState(true);
@@ -16,16 +18,18 @@ function App() {
 		setLoading(false);
 		gsap.from(appRef.current, {
 			opacity: 0,
-			duration: 1,
+			duration: 1.5,
 			ease: "Power3.easeInOut",
 		});
 		// const cursor = new Cursor(cursorRef.current);
 	}, []);
-
+	console.log($(window).height())
 	return (
 		<ContextProvider>
 			<div ref={appRef} className={`App ${loading ? "loading" : ""}`}>
-				<Nav />
+				
+
+				{$(window).height() > 1000 ? <Nav /> : <HamburgerNav />}
 				<Homepage />
 				{/* <div ref={cursorRef} className="cursor"></div> */}
 			</div>
